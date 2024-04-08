4b5af66cab29f666a157148caa14ead8ecf6ddc5
kareemyasser8 <kareem.yasserr@gmail.com>
2024-04-08 15:05:09 +0200

    Improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
 import prisma from "@/prisma/client"
-import IssueForm from "../../_components/IssueForm"
 import { notFound } from "next/navigation"
+import dynamic from "next/dynamic"
+import IssueFormSkeleton from "./loading"
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+})
 
 interface Props {
   params: { id: string }
@@ -11,7 +17,7 @@ const EditIssuePage = async ({ params }: Props) => {
     where: { id: parseInt(params.id) },
   })
 
-  if(!issue) notFound();
+  if (!issue) notFound()
 
   return <IssueForm issue={issue} />
 }
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,25 +1,17 @@
 "use client"
-
-import ErrorMessage from "@/app/components/ErrorMessage"
-import Spinner from "@/app/components/Spinner"
-import { issueSchema } from "@/app/validationSchemas"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button, Callout, TextField } from "@radix-ui/themes"
-import axios from "axios"
-import "easymde/dist/easymde.min.css"
-import dynamic from "next/dynamic"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
-import { Controller, useForm } from "react-hook-form"
-import { z } from "zod"
-import LoadingNewIssuePage from "../new/loading"
-import { Issue } from "@prisma/client"
-// import SimpleMDE from "react-simplemde-editor";
-
-const SimpleMDE = dynamic(async () => import("react-simplemde-editor"), {
-  ssr: false,
-  loading: () => <LoadingNewIssuePage />,
-})
+import 'easymde/dist/easymde.min.css';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { issueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Issue } from '@prisma/client';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+import { z } from 'zod';
 
 type IssueFormData = z.infer<typeof issueSchema>
 
