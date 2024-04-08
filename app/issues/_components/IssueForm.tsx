e476f64e38a09ca15d980dc91c83a8e52d0806d0
kareemyasser8 <kareem.yasserr@gmail.com>
2024-04-08 13:17:41 +0200

    Build an API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -2,7 +2,7 @@
 
 import ErrorMessage from "@/app/components/ErrorMessage"
 import Spinner from "@/app/components/Spinner"
-import { createIssueSchema } from "@/app/validationSchemas"
+import { issueSchema } from "@/app/validationSchemas"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button, Callout, TextField } from "@radix-ui/themes"
 import axios from "axios"
@@ -21,7 +21,7 @@ const SimpleMDE = dynamic(async () => import("react-simplemde-editor"), {
   loading: () => <LoadingNewIssuePage />,
 })
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof issueSchema>
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const {
@@ -30,7 +30,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   })
   const router = useRouter()
   const [error, setError] = useState("")
